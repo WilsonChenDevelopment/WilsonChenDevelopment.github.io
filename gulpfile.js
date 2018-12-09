c67a38c3112a8168a0f96727288aa6c471c90eec
wilson chen <wilson.chen@student.uts.edu.au>
2018-12-09 21:46:36 +1100

    Commit of main website

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+ 
+sass.compiler = require('node-sass');
+
+var date = new Date();
+
+gulp.task('css', function (){
+  return gulp.src('./sass/style.sass')
+  .pipe(sass.sync({
+    outputStyle: 'expanded'
+  }).on('error', sass.logError))
+  .pipe(gulp.dest('./css'))
+})
+
+gulp.task('views', function () {
+  return gulp.src('views/*.pug')
+  .pipe(pug({
+    locals:{
+      currentyear:date.getUTCFullYear()
+    }
+  }))
+  .pipe(gulp.dest('./'))
+});
+
+gulp.task('watch-pug', function(){
+  gulp.watch('views/*.pug', ['views']);
+})
+
+gulp.task('watch-css', function(){
+  gulp.watch('./sass/*.sass', ['css']);
+})
+
+gulp.task('default', ['css', 'views', 'watch-pug', 'watch-css']);
+
